a00ab109e18ad4f0a19dc2d73a99eb7751a2a508
chrismushaz <Mushabati6jr@gmail.com>
2025-06-14 23:14:11 +0200

    Update ALLOWED_HOSTS and template tweaks

--- errandsmate/settings.py
+++ errandsmate/settings.py
@@ -29,7 +29,8 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'https://ristorands.onrender.com').split(',')
+
 
 
 # Application definition
